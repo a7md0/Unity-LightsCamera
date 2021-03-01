aa265e383158af9ea5eae133f98393677da2cb42
a7md0 <a7m3d699@gmail.com>
2021-03-01 13:25:12 +0300

    1st Person Camera script

---
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    private float xInput, yInput;
+    public GameObject camObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xInput = Input.GetAxis("Mouse X"); // left-right
+        yInput = Input.GetAxis("Mouse Y"); // up-down
+
+        camObject.transform.Rotate(new Vector3(-yInput, xInput, 0));
+    }
+}
