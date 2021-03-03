55a6cd8b53998901ce875516e60a47fe51fc06e8
a7md0 <a7m3d699@gmail.com>
2021-03-03 13:06:26 +0300

    Try 3rd person camera

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject cam;
+    private float xInput, yInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Make the camera always face its target
+        cam.transform.LookAt(target.transform);
+
+        //Follow the target around
+        transform.position = target.transform.position;
+
+        //Get mouse input, and rotate accordingly
+        xInput = Input.GetAxis("Mouse X");
+        yInput = -Input.GetAxis("Mouse Y");
+        transform.Rotate(new Vector3(-yInput, xInput, 0));
+    }
+}
