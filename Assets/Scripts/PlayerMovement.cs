3c176d3e5a898eadd402dadf98b3dab53f8eadde
a7md0 <a7m3d699@gmail.com>
2021-03-03 12:13:27 +0300

    Create player movment script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float xInput, zInput;
+    public float speed;
+
+    // Update is called once per frame
+    void Update() {
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(xInput, 0, zInput) * Time.deltaTime * speed);
+    }
+}
